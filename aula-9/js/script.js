56dcc6dd03dd50d2bc7a944d177868517480032b
Guto Xavier <gutoffline@gmail.com>
2020-11-14 17:58:08 -0300

    Aula 9

---
+++ aula-9/js/script.js
@@ -0,0 +1,10 @@
+function entrar(){
+    alert("Então você quer entrar?");
+
+    let usuario = document.querySelector("#usuario").value;
+
+    alert(usuario + " esse é você?");
+
+    let nome = prompt("Coloque seu nome de verdade: ", "Escreva aqui");
+
+}
